1f158edfbd66a668cef4060cf3aab50525e34e8c
melonpan5 <hoshi2771@gmail.com>
2020-05-07 17:03:35 +0900

    [Finish]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '68dd586ad36f49dbc5f459edf0fc68653822b0daab9229f0bdf5cc9629d6c3292ef816705fab7ac754c3552ced2d1e5848606722eded87510447af7257952613'
